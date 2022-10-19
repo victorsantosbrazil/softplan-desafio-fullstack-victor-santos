08d7ab3d9958ca499c433265151005d85bc17b97
Victor Santos Batista <victorsantos.developer@gmail.com>
2022-10-19 19:38:08 -0300

    Project Start

---
+++ process-management-api/src/main/java/com/victorsantos/processmanagementapi/ProcessManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.victorsantos.processmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessManagementApiApplication.class, args);
+	}
+
+}
