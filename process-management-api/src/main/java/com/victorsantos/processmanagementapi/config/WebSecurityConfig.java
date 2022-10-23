724e591ad9018405c6c6ce9ba558ce27bbc08284
Victor Santos Batista <victorsantos.developer@gmail.com>
2022-10-23 18:42:48 -0300

    [BACKEND] Configuring cors to allow all origins and methods

--- process-management-api/src/main/java/com/victorsantos/processmanagementapi/config/WebSecurityConfig.java
+++ process-management-api/src/main/java/com/victorsantos/processmanagementapi/config/WebSecurityConfig.java
@@ -1,11 +1,17 @@
 package com.victorsantos.processmanagementapi.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class WebSecurityConfig {
@@ -22,5 +28,23 @@ public class WebSecurityConfig {
     http.authorizeRequests().anyRequest().permitAll();
     return http.build();
 
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(),
+        HttpMethod.HEAD.name(),
+        HttpMethod.POST.name(),
+        HttpMethod.PATCH.name(),
+        HttpMethod.PUT.name(),
+        HttpMethod.DELETE.name()));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    configuration.setExposedHeaders(Arrays.asList("token"));
+    configuration.setMaxAge(1800L);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
